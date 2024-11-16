64cffde0f4ef8607c0086dbdfcfede3fe759b521
BURAK BAYTEMİR <burakbaytemir@posta.mu.edu.tr>
2024-11-17 01:25:53 +0300

    Add project files.

---
+++ AgriculturePresentation.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgriculturePresentation.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
